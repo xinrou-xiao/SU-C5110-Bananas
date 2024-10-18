6867e9c7e453beff8f9fa59f6bf5493493be1116
greenpoop <apple88501@gmail.com>
2024-10-18 15:16:32 -0700

    Add ReadController.cs under Controllers folder

---
+++ src/Controllers/ReadController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    public class ReadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
