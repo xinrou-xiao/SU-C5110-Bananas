ad33cb07aeebc0b43388e9ffc14b6e47a6d15b0b
greenpoop <apple88501@gmail.com>
2024-10-18 16:36:42 -0700

    Add comment for ReadController on ReadController.cs

--- src/Controllers/ReadController.cs
+++ src/Controllers/ReadController.cs
@@ -4,6 +4,10 @@ namespace ContosoCrafts.WebSite.Controllers
 {
     public class ReadController : Controller
     {
+        /// <summary>
+        /// Read page Controller, return its view
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View();
