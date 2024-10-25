0a262bb363bfb1d40c8d44a84e41e62723228899
greenpoop <apple88501@gmail.com>
2024-10-24 21:57:59 -0700

    Added function Url on OTTModel.cs

--- src/Models/OTTModel.cs
+++ src/Models/OTTModel.cs
@@ -6,5 +6,10 @@
         /// Map Platform field of OTT object in product.json to OTTModel's Platform
         /// </summary>
         public string Platform { get; set; }
+
+        /// <summary>
+        /// Map Url field of OTT object in product.json to OTTModel's Url
+        /// </summary>
+        public string Url { get; set; }
     }
 }
