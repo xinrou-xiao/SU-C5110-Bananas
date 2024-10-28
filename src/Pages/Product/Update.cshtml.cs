e44502163303362ffe9c40fc3c72b4ab61ee813c
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-10-28 01:57:02 -0700

    Created Update.cshtml.cs file for implement update functionality

---
+++ src/Pages/Product/Update.cshtml.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    public class UpdateModel : PageModel
+    {
+        private readonly JsonFileProductService _productService;
+
+        public UpdateModel(JsonFileProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [BindProperty]
+        public ProductModel Product { get; set; }
+
+        public IActionResult OnGet(string id)
+        {
+            Product = _productService.GetOneDataById(id);
+            if (Product ==null)
+            {
+                return RedirectToPage("/Error");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var updatedProduct = _productService.UpdateData(Product);
+            if (updatedProduct ==null)
+            {
+                return RedirectToPage("/Error");
+            }
+
+            return RedirectToPage("/Product/Index");
+        }
+    }
+}
