c7980ebb379e3d3333dc140f08ee42379db40307
Samarth Tanwar <stanwar@seattleu.edu>
2024-10-29 21:56:32 -0700

    Updated the character limit in ProductModel.cs file

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -15,7 +15,7 @@ namespace ContosoCrafts.WebSite.Models
 
         public string Url { get; set; }
         
-        [StringLength (maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
+        [StringLength (maximumLength: 200, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
         public string Title { get; set; }
 
         public string Description { get; set; }
