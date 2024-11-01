590e6a426481db5dad39fa9e7c57adf14d5abfeb
greenpoop <apple88501@gmail.com>
2024-11-01 16:48:07 -0700

    Added click event binding for id" create-genre" button on site.js

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+/// onClick event for create genre button, will append a new genre input group after create button,
+/// bind remove function to remove button for new Added input group
+$("#create-genre").click(function () {
+    $(this).after('<div class="form-group form-inline">' +
+        '<input class="form-control" type="text" name="genre_dynamic" placeholder="Genre" />' +
+        '<button class="btn btn-danger custom-btn remove-genre" type="button">-</button>' +
+        '<span asp-asp-validation-for="Product.Genre" class="text-danger"></span>' +
+        '</div >')
+    $(".remove-genre").click(function () {
+        $(this).parent().remove()
+    })
+})
