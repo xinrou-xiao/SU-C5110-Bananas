bcd67ea9b53a32c7a7059347845a1bd7abf60e19
greenpoop <apple88501@gmail.com>
2024-11-01 16:51:04 -0700

    Added click event for id "create-OTT" button on site.js

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -20,3 +20,26 @@ $("#create-genre").click(function () {
 $(".remove-genre").click(function () {
     $(this).parent().remove()
 })
+
+/// bind onClick event for id is create-OTT button, append a OTT input group after create button,
+/// bind remove function to remove button for new Added input group
+$("#c").click(function () {
+    $(this).after('<div class="form-group border border-light border-right-0 border-left-0 rounded OTT-group">' +
+        '<div class= "form-group"> ' +
+        '<label class="white-font" >Platform</label>' +
+        '<button class="btn btn-danger custom-btn remove-OTT" type="button">-</button>' +
+        '<input class="form-control" type="text" name="OTT_dynamic_platform" placeholder="Platform"/>' +
+        ' </div >' +
+        ' <div class="form-group">' +
+        '<label class="white-font">Url</label>' +
+        '<input class="form-control" type="text" name="OTT_dynamic_url" placeholder="Platform URL" />' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label class="white-font">Icon</label>' +
+        '<input class="form-control" type="text" name="OTT_dynamic_icon" placeholder="Platform Icon" />' +
+        '</div>' +
+        '</div >')
+    $(".remove-OTT").click(function () {
+        $(this).parent().parent().remove()
+    })
+})
