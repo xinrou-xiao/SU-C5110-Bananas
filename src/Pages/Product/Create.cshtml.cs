7f5d579ea1f3cf0219f9f83982c2e82c26413272
greenpoop <apple88501@gmail.com>
2024-10-31 20:29:29 -0700

    Added Create.cshtml and Create.cshtml.cs(they were created together) under Product folder

---
+++ src/Pages/Product/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
