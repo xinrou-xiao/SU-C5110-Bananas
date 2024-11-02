4711c92801ed515024bc43c95eed6b5417a68f6a
ProZone <ssinha1@seattleu.edu>
2024-11-02 03:32:51 -0700

    Updated Configure method to eliminate else statement in Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -34,12 +34,11 @@ namespace ContosoCrafts.WebSite
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
+  
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
+            
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
