346e079bc3b0de162b23308506ac8d30c03ff437
ProZone <ssinha1@seattleu.edu>
2024-11-02 03:03:13 -0700

    Added comments to CreateHostBuilder method in Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -16,14 +16,22 @@ namespace ContosoCrafts.WebSite
         /// <param name="args">The command-line arguments passed to the application.</param>
         public static void Main(string[] args)
         {
+
             // Create the host builder, build it, and run the application
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Creates a host builder for the web application with default settings.
+        /// Configures the web host to use the specified Startup class.
+        /// </summary>
+        /// <param name="args">The command-line arguments passed to the application.</param>
+        /// <returns>An IHostBuilder instance configured for the application.</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    // Specify the Startup class to be used for configuring services and the request pipeline
                     webBuilder.UseStartup<Startup>();
                 });
     }
