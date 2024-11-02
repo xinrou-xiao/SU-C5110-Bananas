005888b0561b4b6c7795c3a56e046112608998f1
Samarth Tanwar <stanwar@seattleu.edu>
2024-11-02 13:28:45 -0700

    Added code to retrieve data for delete in Delete.cshtml.cs file

--- src/Pages/Product/Delete.cshtml.cs
+++ src/Pages/Product/Delete.cshtml.cs
@@ -1,12 +1,43 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        /// Default Constructor
+        public DeleteModel(JsonFileProductService productService)
         {
+            ProductService = productService;
+        }
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+
+        // Product data
+        public ProductModel Product { get; private set; }
+
+        public void OnGet(string id)
+        {
+            // Set Product to null and return
+            if (id == null)
+            {
+                Product = null;
+                return;
+            }
+            Product = ProductService.GetOneDataById(id);
+        }
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+            ProductService.DeleteData(Product.Id);
+            return RedirectToPage("./Index");
+
         }
     }
 }
