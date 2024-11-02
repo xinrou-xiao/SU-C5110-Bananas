264f48427031f8d9b29c49e9b2c5175e544d75ea
ProZone <ssinha1@seattleu.edu>
2024-11-02 01:08:06 -0700

    Added comments for the IndexModel class and constructor in Pages/Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -8,23 +8,27 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+
     /// <summary>
-    /// Xinrou Xiao
-    /// Samarth Singh Tanwar
-    /// Vineet Somwanshi
-    /// Shanvi Sinha
-    /// 10/02 10:24pm Xinrou test github push on mac
-    /// Shanvi testing 
+    /// Model for the Index page that handles the retrieval and display of product data.
     /// </summary>
     public class IndexModel : PageModel
     {
-        //Hi Mike
+        // Logger to log messages for this page
         private readonly ILogger<IndexModel> _logger;
 
+        /// <summary>
+        /// Constructor to initialize the IndexModel with a logger and product service.
+        /// </summary>
+        /// <param name="logger">The logger used for logging information.</param>
+        /// <param name="productService">Service to retrieve product data.</param>
         public IndexModel(ILogger<IndexModel> logger,
+
             JsonFileProductService productService)
         {
+            // Assign the logger
             _logger = logger;
+            // Assign the product service
             ProductService = productService;
         }
 
