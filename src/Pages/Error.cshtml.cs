93f09cd98386969f94ffd2ead5a0388783c577c8
ProZone <ssinha1@seattleu.edu>
2024-11-02 00:53:57 -0700

    Added comments for the ErrorModel class and its constructor in Pages/Error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -6,6 +6,10 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+
+    /// <summary>
+    /// Model for handling error pages and displaying error information.
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
@@ -13,8 +17,13 @@ namespace ContosoCrafts.WebSite.Pages
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        // Logger to record error information for debugging purposes
         private readonly ILogger<ErrorModel> _logger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ErrorModel"/> class with a specified logger.
+        /// </summary>
+        /// <param name="logger">The logger used to log error information.</param>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
