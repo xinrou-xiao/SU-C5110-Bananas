d59f5a0cc9f1303f603512cd48731635eab4bfdd
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-11-02 22:21:43 -0700

    Added comments for the code in ReadController.cs file

--- src/Controllers/ReadController.cs
+++ src/Controllers/ReadController.cs
@@ -1,15 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc; // Importing ASP.NET Core MVC components
 
-namespace ContosoCrafts.WebSite.Controllers
+namespace ContosoCrafts.WebSite.Controllers // Defining the namespace for the controller
 {
+    // Controller for handling read page requests
     public class ReadController : Controller
     {
         /// <summary>
-        /// Read page Controller, return its view
+        /// Action method to return the Read page view.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>An IActionResult representing the Read page view.</returns>
         public IActionResult Read()
         {
+            // Returning the view associated with the Read action
             return View();
         }
     }
