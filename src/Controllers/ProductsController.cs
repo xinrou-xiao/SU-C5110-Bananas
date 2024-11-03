f64af0b7427203e9974c2898f2b1a1183cd650a1
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-11-02 21:42:11 -0700

    Added comments for the code for ProductsController.cs file

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -1,26 +1,30 @@
-using System.Collections.Generic;
+using System.Collections.Generic; // Importing the namespace for collections
+using Microsoft.AspNetCore.Mvc; // Importing ASP.NET Core MVC components
+using ContosoCrafts.WebSite.Models; // Importing the models used in the application
+using ContosoCrafts.WebSite.Services; // Importing the services used for product management
 
-using Microsoft.AspNetCore.Mvc;
-
-using ContosoCrafts.WebSite.Models;
-using ContosoCrafts.WebSite.Services;
-
-namespace ContosoCrafts.WebSite.Controllers
+namespace ContosoCrafts.WebSite.Controllers // Defining the namespace for the controller
 {
+    // Specifying that this class is an API controller
     [ApiController]
+    // Defining the route for the controller, which will be prefixed with "products"
     [Route("[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : ControllerBase // Inheriting from ControllerBase for API functionality
     {
+        // Constructor that takes a JsonFileProductService as a dependency
         public ProductsController(JsonFileProductService productService)
         {
-            ProductService = productService;
+            ProductService = productService; // Assigning the injected service to a property
         }
 
+        // Property to hold the product service instance
         public JsonFileProductService ProductService { get; }
 
+        // HTTP GET method to retrieve all products
         [HttpGet]
         public IEnumerable<ProductModel> Get()
         {
+            // Calling the service to get all product data and returning it
             return ProductService.GetAllData();
         }
 
