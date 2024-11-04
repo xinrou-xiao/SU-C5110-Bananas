c3eeb21ac6ad477770867732748b88f1cb67f3f4
ProZone <ssinha1@seattleu.edu>
2024-11-04 13:27:20 -0800

    Added comments to UnitTests file Components/ProductList.razor.Tests.cs

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -14,9 +14,11 @@ namespace UnitTests.Components
     {
         #region TestSetup
 
+        // Method to set up any initial configurations or services needed for tests
         [SetUp]
         public void TestInitialize()
         {
+        // Currently, nothing to initialize, but method is here if setup is needed later
         }
 
         #endregion TestSetup
@@ -25,15 +27,18 @@ namespace UnitTests.Components
         public void ProductList_Valid_Default_Should_Return_Content()
         {
             // Arrange
+            // Registering JsonFileProductService as a singleton service, so it can be used in the test
             Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
 
             // Act
+            // Rendering the ProductList component and storing the result
             var page = RenderComponent<ProductList>();
 
             // Get the Cards retrned
             var result = page.Markup;
 
             // Assert
+            // Verifying that the rendered markup contains a specific product title, "Naruto: The Journey of a Ninja Dreamer"
             Assert.That(result.Contains("Naruto: The Journey of a Ninja Dreamer"), Is.EqualTo(true));
         }
     }
