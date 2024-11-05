f8731aa3a553bfa99da0f911447f3d00c3f41db3
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-11-04 22:18:15 -0800

    Updated Unit test code for CommentModel.cs.Tests.cs file

--- UnitTests/Models/CommentModel.cs.Tests.cs
+++ UnitTests/Models/CommentModel.cs.Tests.cs
@@ -8,32 +8,36 @@ using System.Threading.Tasks;
 
 namespace UnitTests.Models
 {
+    [TestFixture]
     public class CommentModelTests
     {
-        public void Constructor_Should_Initialize_Id_With_New_Guid()
+        [Test]
+        public void Constructor_Should_Initialize_Id_With_Unique_Value()
         {
             // Arrange & Act
-            var commentModel = new CommentModel();
+            var comment1 = new CommentModel();
+            var comment2 = new CommentModel();
 
             // Assert
-            Assert.That(commentModel.Id, Is.Not.Null);
-            Assert.That(commentModel.Id, Is.Not.Empty);
-            Assert.That(System.Guid.TryParse(commentModel.Id, out _), Is.True);
+            Assert.That(comment1.Id, Is.Not.Null.And.Not.Empty, "Expected Id to be initialized with a non-null, non-empty value.");
+            Assert.That(comment2.Id, Is.Not.Null.And.Not.Empty, "Expected Id to be initialized with a non-null, non-empty value.");
+            Assert.That(comment1.Id, Is.Not.EqualTo(comment2.Id), "Expected each instance to have a unique Id.");
         }
+
         [Test]
         public void Comment_Property_Should_Be_Set_And_Retrieved_Correctly()
         {
             // Arrange
-            var commentModel = new CommentModel();
+            var comment = new CommentModel();
             var expectedComment = "This is a test comment.";
 
             // Act
-            commentModel.Comment = expectedComment;
+            comment.Comment = expectedComment;
 
             // Assert
-            Assert.That(commentModel.Comment, Is.EqualTo(expectedComment));
+            Assert.That(comment.Comment, Is.EqualTo(expectedComment), "Expected Comment property to be set and retrieved correctly.");
+
+
         }
-
-
     }
 }
