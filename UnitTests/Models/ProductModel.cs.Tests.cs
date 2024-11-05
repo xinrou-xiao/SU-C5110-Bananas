d431cee32aae446ff0bde90ac08ec510a122c9a8
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-11-04 16:38:34 -0800

    Added unit test for Image_Property_Should_Be_Set_And_Retrieved_Correctly() in ProductModel.cs.Tests.cs file

--- UnitTests/Models/ProductModel.cs.Tests.cs
+++ UnitTests/Models/ProductModel.cs.Tests.cs
@@ -37,7 +37,19 @@ namespace UnitTests.Models
             // Assert
             Assert.That(productModel.Maker, Is.EqualTo(expectedMaker));
         }
+        [Test]
+        public void Image_Property_Should_Be_Set_And_Retrieved_Correctly()
+        {
+            // Arrange
+            var productModel = new ProductModel();
+            var expectedImage = "image.png";
 
+            // Act
+            productModel.Image = expectedImage;
+
+            // Assert
+            Assert.That(productModel.Image, Is.EqualTo(expectedImage));
+        }
 
     }
 }
