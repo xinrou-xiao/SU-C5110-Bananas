8e1b64c799dcffc4a39a3a2588cfb82e172c6451
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-11-04 19:16:45 -0800

    Added unit test Setup() code for  ProductsController.cs.Tests.cs file

--- UnitTests/Controllers/ProductsController.cs.Tests.cs
+++ UnitTests/Controllers/ProductsController.cs.Tests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ContosoCrafts.WebSite.Controllers;
+using ContosoCrafts.WebSite.Services;
+using Moq;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,20 @@ using System.Threading.Tasks;
 
 namespace UnitTests.Controllers
 {
-    internal class ProductsController
+    [TestFixture]
+    public class ProductsControllerTests
     {
+        private Mock<JsonFileProductService> _mockProductService;
+        private ProductsController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initialize the mock service
+            _mockProductService = new Mock<JsonFileProductService>();
+
+            // Create the controller with the mocked service
+            _controller = new ProductsController(_mockProductService.Object);
+        }
     }
 }
