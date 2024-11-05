13a02fb21285090e1efaa5cf3c2b5df302312c4b
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-11-04 16:23:55 -0800

    Added CommentModel.cs.Tests.cs file under UnitTests/Models for unit testing

---
+++ UnitTests/Models/CommentModel.cs.Tests.cs
@@ -0,0 +1,26 @@
+﻿using ContosoCrafts.WebSite.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Models
+{
+    public class CommentModelTests
+    {
+        public void Constructor_Should_Initialize_Id_With_New_Guid()
+        {
+            // Arrange & Act
+            var commentModel = new CommentModel();
+
+            // Assert
+            Assert.That(commentModel.Id, Is.Not.Null);
+            Assert.That(commentModel.Id, Is.Not.Empty);
+            Assert.That(System.Guid.TryParse(commentModel.Id, out _), Is.True);
+        }
+
+        
+    }
+}
