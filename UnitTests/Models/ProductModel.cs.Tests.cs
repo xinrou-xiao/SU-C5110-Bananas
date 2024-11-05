f48f8880e2210a27784783c0734e6faa862a6996
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-11-04 16:36:54 -0800

    Added Unit tests for Id_Property_Should_Be_Set_And_Retrieved_Correctly() in ProductModel.cs.Tests.cs file

--- UnitTests/Models/ProductModel.cs.Tests.cs
+++ UnitTests/Models/ProductModel.cs.Tests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ContosoCrafts.WebSite.Models;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,22 @@ using System.Threading.Tasks;
 
 namespace UnitTests.Models
 {
-    internal class ProductModel
+    [TestFixture]
+    public class ProductModelTests
     {
+        [Test]
+        public void Id_Property_Should_Be_Set_And_Retrieved_Correctly()
+        {
+            // Arrange
+            var productModel = new ProductModel();
+            var expectedId = "123";
+
+            // Act
+            productModel.Id = expectedId;
+
+            // Assert
+            Assert.That(productModel.Id, Is.EqualTo(expectedId));
+        }
+
     }
 }
