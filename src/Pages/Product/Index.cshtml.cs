a5382a166c63234eb4dbc380cf62554d80fa7021
Samarth Tanwar <stanwar@seattleu.edu>
2024-11-08 14:44:52 -0800

    Updated coding style in Index.cshtml.cs file

--- src/Pages/Product/Index.cshtml.cs
+++ src/Pages/Product/Index.cshtml.cs
@@ -9,31 +9,39 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages.Product
 {
     /// <summary>
-    /// Index Page will return all the data to show the user
+    /// Index Page will return all the data to show the user.
     /// </summary>
     public class IndexModel : PageModel
     {
         /// <summary>
-        /// Default Constructor
+        /// Default Constructor.
         /// </summary>
-        /// <param name="productService"></param>
+        /// <param name="productService">Service to retrieve product data.</param>
         public IndexModel(JsonFileProductService productService)
         {
+            // Initialize product service
             ProductService = productService;
         }
 
         // Data Service
-        public JsonFileProductService ProductService { get; }
+        public JsonFileProductService ProductService
+        {
+            get;
+        }
 
         // Collection of the Data
-        public IEnumerable<ProductModel> Products { get; private set; }
+        public IEnumerable<ProductModel> Products
+        {
+            get; private set;
+        }
 
         /// <summary>
-        /// REST OnGet
-        /// Return all the data
+        /// REST OnGet.
+        /// Return all the data.
         /// </summary>
         public void OnGet()
         {
+            // Retrieve all products
             Products = ProductService.GetAllData();
         }
     }
