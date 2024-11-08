9f122fc137f031a8bde9ed00661d0fca6178aeab
Samarth Tanwar <stanwar@seattleu.edu>
2024-11-08 15:53:18 -0800

    Updated Coding style to Create.cshtml.cs file

--- src/Pages/Product/Create.cshtml.cs
+++ src/Pages/Product/Create.cshtml.cs
@@ -1,29 +1,33 @@
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
+    /// <summary>
+    /// CreateModel class for handling creation of new product data.
+    /// </summary>
     public class CreateModel : PageModel
     {
         /// <summary>
-        /// Default Constructor
+        /// Default Constructor.
         /// </summary>
-        /// <param name="productService"></param>
+        /// <param name="productService">Service for managing product data.</param>
         public CreateModel(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
-        // Data Service
+        // Data service for accessing product data
         public JsonFileProductService ProductService { get; }
 
         // Product data
         public ProductModel Product { get; private set; }
 
         /// <summary>
-        /// Initialize Product object.
+        /// Initializes Product object.
         /// </summary>
         public void OnGet()
         {
@@ -31,41 +35,48 @@ namespace ContosoCrafts.WebSite.Pages.Product
         }
 
         /// <summary>
-        /// Create page post receiver, will skip empty data in genre_dynamic and OTT_dynamic_platform,
-        /// and set up data into Product object, then call CreateData to add this new data to json file.
+        /// Processes POST requests to create a new product entry.
         /// </summary>
-        /// <param name="Product"></param>
-        /// <param name="genre_dynamic">genere string array</param>
-        /// <param name="OTT_dynamic_platform">platfrom name string array</param>
-        /// <param name="OTT_dynamic_url">platfrom url string array</param>
-        /// <param name="OTT_dynamic_icon">platfrom icon string array</param>
-        /// <returns></returns>
-        public IActionResult OnPost(ProductModel Product, string[] genre_dynamic, string[] OTT_dynamic_platform, string[] OTT_dynamic_url, string[] OTT_dynamic_icon)
+        /// <param name="Product">The product to create.</param>
+        /// <param name="genre_dynamic">Genre string array.</param>
+        /// <param name="OTT_dynamic_platform">Platform name string array.</param>
+        /// <param name="OTT_dynamic_url">Platform URL string array.</param>
+        /// <param name="OTT_dynamic_icon">Platform icon string array.</param>
+        /// <returns>Redirects to the Index page on success.</returns>
+        public IActionResult OnPost(
+            ProductModel Product,
+            string[] genre_dynamic,
+            string[] OTT_dynamic_platform,
+            string[] OTT_dynamic_url,
+            string[] OTT_dynamic_icon)
         {
-            // Return to update page if model state is invalid
+            // Redirect to error page if model state is invalid
             if (!ModelState.IsValid)
             {
                 return RedirectToPage("/Error");
             }
 
-            Product.Id = System.Guid.NewGuid().ToString(); // set up id
-            Product.Maker = "banana"; // maker is us
+            // Generate unique ID and set maker for the new product
+            Product.Id = System.Guid.NewGuid().ToString();
+            Product.Maker = "banana";
 
-            if (genre_dynamic.Length > 0) // if genre_dynamic has data inside
+            // Process genre_dynamic array if it contains data
+            if (genre_dynamic.Length > 0)
             {
-                int count_Valid = 0; // to count how many valid gnere are valid
+                int count_Valid = 0;
+
+                // Count non-null entries in genre_dynamic
                 for (int i = 0; i < genre_dynamic.Length; i++)
                 {
-                    // if genre is not null, count++
                     if (genre_dynamic[i] != null)
                     {
                         count_Valid++;
                     }
                 }
 
-                // set up Product.Genre to a correct length's array
+                // Initialize Product.Genre with valid entries
                 Product.Genre = new string[count_Valid];
-                var index = 0; // position in Gnere
+                var index = 0;
 
                 for (int i = 0; i < genre_dynamic.Length; i++)
                 {
@@ -77,23 +88,29 @@ namespace ContosoCrafts.WebSite.Pages.Product
                 }
             }
 
-            // if platform name has data inside
+            // Process OTT_dynamic arrays if platform names are provided
             if (OTT_dynamic_platform.Length > 0)
             {
                 for (int i = 0; i < OTT_dynamic_platform.Length; i++)
                 {
-                    if (OTT_dynamic_platform[i] != null) // must at least provide platform name
+                    if (OTT_dynamic_platform[i] != null) // Ensure platform name is provided
                     {
-                        OTTModel OTT = new OTTModel();
-                        OTT.Platform = OTT_dynamic_platform[i];
-                        OTT.Url = OTT_dynamic_url[i];
-                        OTT.Icon = OTT_dynamic_icon[i];
+                        OTTModel OTT = new OTTModel
+                        {
+                            Platform = OTT_dynamic_platform[i],
+                            Url = OTT_dynamic_url[i],
+                            Icon = OTT_dynamic_icon[i]
+                        };
+
                         Product.OTT.Add(OTT);
                     }
                 }
             }
-            // Create new data and insert to json file
+
+            // Create new product data and save to JSON file
             ProductService.CreateData(Product);
+
+            // Redirect to the Product Index page after successful creation
             return RedirectToPage("/Product/Index");
         }
     }
