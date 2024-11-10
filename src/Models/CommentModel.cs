af0157b130f4ed162569a60584537026002c4f98
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-11-09 19:35:47 -0800

    Updated coding style in CommentModel.cs

--- src/Models/CommentModel.cs
+++ src/Models/CommentModel.cs
@@ -1,15 +1,14 @@
-
 namespace ContosoCrafts.WebSite.Models
 {
     /// <summary>
-    /// Comments entered by the user about the Product
+    /// Comments entered by the user about the Product.
     /// </summary>
     public class CommentModel
     {
-        // The ID for this comment, use a Guid so it is always unique
+        // The ID for this comment, use a Guid so it is always unique.
         public string Id { get; set; } = System.Guid.NewGuid().ToString();
 
-        // The Comment
+        // The Comment.
         public string Comment { get; set; }
     }
 }
