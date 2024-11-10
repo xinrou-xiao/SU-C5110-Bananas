6813cce54e682c1127d4bd859ccd6fbb7adfe356
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-11-09 17:59:05 -0800

    Updated coding style in ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -1,7 +1,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoCrafts.WebSite.Models
 {
@@ -10,47 +10,32 @@ namespace ContosoCrafts.WebSite.Models
     /// </summary>
     public class ProductModel
     {
-        /// <summary>
-        /// Map Id field in product.json to ProductModel's Id
-        /// </summary>
+        // Map Id field in product.json to ProductModel's Id
         public string Id { get; set; }
 
-        /// <summary>
-        /// Map Maker field in product.json to ProductModel's Maker
-        /// </summary>
+        // Map Maker field in product.json to ProductModel's Maker
         public string Maker { get; set; }
 
-        /// <summary>
-        /// Map img field in product.json to ProductModel's Image
-        /// </summary>
+        // Map img field in product.json to ProductModel's Image
         [JsonPropertyName("img")]
         public string Image { get; set; }
 
-        /// <summary>
-        /// Map Url field in product.json to ProductModel's Url
-        /// </summary>
+        // Map Url field in product.json to ProductModel's Url
         public string Url { get; set; }
 
-        // <summary>
-        /// Map Title field in product.json to ProductModel's Title with validation
-        /// </summary>
-        [StringLength (maximumLength: 200, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
+        // Map Title field in product.json to ProductModel's Title with validation
+        [StringLength(maximumLength: 200, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
         public string Title { get; set; }
 
-        /// <summary>
-        /// Map Description field in product.json to ProductModel's Description
-        /// </summary>
+        // Map Description field in product.json to ProductModel's Description
         public string Description { get; set; }
 
-        /// <summary>
-        /// Map Ratings field in product.json to ProductModel's Ratings array
-        /// </summary>
-        public int[] Ratings { get; set; }
+        // Map Ratings field in product.json to ProductModel's Ratings array
+        public int Ratings { get; set; }
 
-        /// <summary>
-        /// Map Genre field in product.json to ProductModel's Genre
-        /// </summary>
-        public string[] Genre { get; set; }
+        // Map Genre field in product.json to ProductModel's Genre
+        public string Genre { get; set; }
+
 
         /// <summary>
         /// Map Release field in product.json to ProductModel's Release
