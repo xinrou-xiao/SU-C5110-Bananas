902ec2dcc2af9caf94342326c36fa2cfcb873de5
ProZone <ssinha1@seattleu.edu>
2024-11-10 22:34:40 -0800

    Updated coding style in Pages/Product/Index.cshtml.cs file

--- src/Pages/Product/Index.cshtml.cs
+++ src/Pages/Product/Index.cshtml.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 
