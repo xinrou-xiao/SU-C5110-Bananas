cb39ade6f7551fcf7f969a1f96e71b8ec6997c7c
Samarth Tanwar <stanwar@seattleu.edu>
2024-11-15 13:24:11 -0800

    Updated comments in ProductTypeEnum.cs file

--- src/Models/ProductTypeEnum.cs
+++ src/Models/ProductTypeEnum.cs
@@ -5,47 +5,47 @@ using System.Threading.Tasks;
 
 namespace ContosoCrafts.WebSite.Models
 {
-    // <summary>
-    // Types of products available.
-    // </summary>
+    /// <summary>
+    /// Types of products available.
+    /// </summary>
     public enum ProductTypeEnum
     {
-        // <summary>
-        // Default undefined product type.
-        // </summary>
+        /// <summary>
+        /// Default undefined product type.
+        /// </summary>
         Undefined = 0,
 
-        // <summary>
-        // Represents amateur, handmade items.
-        // </summary>
+        /// <summary>
+        /// Represents amateur, handmade items.
+        /// </summary>
         Amature = 1,
 
-        // <summary>
-        // Represents antique items.
-        // </summary>
+        /// <summary>
+        /// Represents antique items.
+        /// </summary>
         Antique = 5,
 
-        // <summary>
-        // Represents collectible items.
-        // </summary>
+        /// <summary>
+        /// Represents collectible items.
+        /// </summary>
         Collectable = 130,
 
-        // <summary>
-        // Represents commercial goods.
-        // </summary>
+        /// <summary>
+        /// Represents commercial goods.
+        /// </summary>
         Commercial = 55,
     }
 
-    // <summary>
-    // Extension methods for the ProductTypeEnum to provide additional functionality.
-    // </summary>
+    /// <summary>
+    /// Extension methods for the ProductTypeEnum to provide additional functionality.
+    /// </summary>
     public static class ProductTypeEnumExtensions
     {
-        // <summary>
-        // Gets the display name for each ProductTypeEnum value.
-        // </summary>
-        // <param name="data">The product type enum value.</param>
-        // <returns>String representing the display name of the product type.</returns>
+        /// <summary>
+        /// Gets the display name for each ProductTypeEnum value.
+        /// </summary>
+        /// <param name="data">The product type enum value.</param>
+        /// <returns>String representing the display name of the product type.</returns>
         public static string DisplayName(this ProductTypeEnum data)
         {
             return data switch
