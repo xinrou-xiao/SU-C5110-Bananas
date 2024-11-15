32219d8ab52080483782c44ce90ca58a4767b3c5
Samarth Tanwar <stanwar@seattleu.edu>
2024-11-15 14:04:43 -0800

    Adeed Developers.cshtml.cs file in src\Pages folder

---
+++ src/Pages/Developers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class DevlopersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
