1a9df3ef12c1bbeb663dd100e67d3ae61a9cfb6a
ProZone <ssinha1@seattleu.edu>
2024-11-15 15:51:04 -0800

    Added Merch.cshtml.cs file in src/Pages folder

---
+++ src/Pages/Merch.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class MerchModel : PageModel
+    {
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
