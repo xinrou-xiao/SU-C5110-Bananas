9005f8cb5e67a276efee30b93c8c2fba213b8ab9
ProZone <ssinha1@seattleu.edu>
2024-11-15 18:41:00 -0800

    Added a new MerchandiseItem in Merch.cshtml.cs

--- src/Pages/Merch.cshtml.cs
+++ src/Pages/Merch.cshtml.cs
@@ -14,7 +14,7 @@ namespace ContosoCrafts.WebSite.Pages
                 new MerchandiseItem
                 {
                     Title = "Luffy Straw Hat",
-                    Description = "More than just headwear... join the straw hat crew.",
+                    Description = "More than just headwear... become part of the straw hat crew.",
                     ImageUrl = "https://store.crunchyroll.com/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dw043745ff/images/3665361049524_one-piece-luffy-straw-hat_01.jpg",
                     Hashtags = new List<string> { "OnePiece", "Pirate", "hat" },
                     BuyLink = "https://store.crunchyroll.com/products/one-piece-luffy-straw-hat-3665361049524.html",
@@ -23,12 +23,22 @@ namespace ContosoCrafts.WebSite.Pages
                 new MerchandiseItem
                 {
                      Title = "Demon Slayer Sword",
-                    Description = "Unleash your inner demon slayer with this katana",
+                    Description = "Unleash your inner demon slayer with this battle-ready katana",
                     ImageUrl = "https://makotoswords.com/cdn/shop/files/Kamado_Tanjirou3_4156966b-6411-453d-b7f9-a5a5279a0b77_1080x.jpg?v=1690178718",
                     Hashtags = new List<string> { "DemonSlayer", "Katana"},
                     BuyLink = "https://makotoswords.com/products/handmade-katana-cosplay-replica-anime-swords?currency=USD&variant=44377331433686&utm_source=google&utm_medium=cpc&utm_campaign=Google%20Shopping&stkn=dcd0a93543e3&gad_source=1&gclid=Cj0KCQiA_9u5BhCUARIsABbMSPvGIUPfQp0mWSr2Hw9mqSwMWbFXq0uQWRQwot6i7JxkEtwCF1GNggUaAh-gEALw_wcB",
                     Price = 19.99m
                 },
+                new MerchandiseItem
+                {
+                     Title = "Gojo Motion Sticker",
+                    Description = "Bring Gojo’s domain expansion to life with this motion sticker",
+                    ImageUrl = "https://seakoff.com/cdn/shop/files/ultimated-void-motion-sticker-578335.gif?v=1711421272&width=1500",
+                    Hashtags = new List<string> { "JujutsuKaisen", "Gojo"},
+                    BuyLink = "https://makotoswords.com/products/handmade-katana-cosplay-replica-anime-swords?currency=USD&variant=44377331433686&utm_source=google&utm_medium=cpc&utm_campaign=Google%20Shopping&stkn=dcd0a93543e3&gad_source=1&gclid=Cj0KCQiA_9u5BhCUARIsABbMSPvGIUPfQp0mWSr2Hw9mqSwMWbFXq0uQWRQwot6i7JxkEtwCF1GNggUaAh-gEALw_wcB",
+                    Price = 3.00m
+                },
+
             };
         }
     }
