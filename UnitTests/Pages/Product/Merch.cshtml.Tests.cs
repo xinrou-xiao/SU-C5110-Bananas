525e53be94bc5e7e18f5cab94ee77ae6db69f1d9
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-11-17 17:20:59 -0800

    Created Unit tests in Merch.cshtml.Tests.cs file

--- UnitTests/Pages/Product/Merch.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Merch.cshtml.Tests.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using ContosoCrafts.WebSite.Pages;
+using NUnit.Framework;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Moq;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
-namespace UnitTests.Pages.Product
+namespace UnitTests.Pages
 {
-    internal class Merch
+    /// <summary>
+    /// Unit tests for the MerchModel class.
+    /// </summary>
+    [TestFixture]
+    public class MerchModelTests
     {
+        #region TestSetup
+
+        public static DefaultHttpContext httpContextDefault;
+        public static ModelStateDictionary modelState;
+        public static ActionContext actionContext;
+        public static EmptyModelMetadataProvider modelMetadataProvider;
+        public static ViewDataDictionary viewData;
+        public static TempDataDictionary tempData;
+        public static PageContext pageContext;
+
+        public static MerchModel PageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            httpContextDefault = new DefaultHttpContext();
+
+            modelState = new ModelStateDictionary();
+
+            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
+
+            modelMetadataProvider = new EmptyModelMetadataProvider();
+            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
+
+            pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData,
+            };
+
+            PageModel = new MerchModel()
+            {
+                PageContext = pageContext,
+                TempData = tempData
+            };
+        }
+
+        #endregion TestSetup
+
+
     }
 }
