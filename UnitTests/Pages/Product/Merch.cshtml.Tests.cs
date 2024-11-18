14601870259973aa4a617ea9f37245c3e9e2b940
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-11-17 17:23:18 -0800

    Created Unit tests in Merch.cshtml.Tests.cs file

--- UnitTests/Pages/Product/Merch.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Merch.cshtml.Tests.cs
@@ -55,7 +55,53 @@ namespace UnitTests.Pages
         }
 
         #endregion TestSetup
+        #region OnGet Tests
 
+        /// <summary>
+        /// Test to verify that the OnGet method initializes the Merchandise list correctly.
+        /// </summary>
+        [Test]
+        public void OnGet_Should_Initialize_Merchandise_List_Correctly()
+        {
+            // Act
+            PageModel.OnGet();
 
+            // Assert
+            Assert.That(PageModel.Merchandise, Is.Not.Null);
+            Assert.That(PageModel.Merchandise.Count, Is.EqualTo(8));
+
+            // Verify each merchandise item
+            Assert.That(PageModel.Merchandise[0].Title, Is.EqualTo("Luffy Straw Hat"));
+            Assert.That(PageModel.Merchandise[1].Title, Is.EqualTo("Demon Slayer Sword"));
+            Assert.That(PageModel.Merchandise[2].Title, Is.EqualTo("Gojo Motion Sticker"));
+            Assert.That(PageModel.Merchandise[3].Title, Is.EqualTo("Hunter x Hunter License"));
+            Assert.That(PageModel.Merchandise[4].Title, Is.EqualTo("Itachi Sharingan Sneakers"));
+            Assert.That(PageModel.Merchandise[5].Title, Is.EqualTo("Ichigo Kurosaki Figure"));
+            Assert.That(PageModel.Merchandise[6].Title, Is.EqualTo("Haikyuu 'Karasuno' Jersey"));
+            Assert.That(PageModel.Merchandise[7].Title, Is.EqualTo("Death Note Diary"));
+        }
+
+        /// <summary>
+        /// Test to verify that each MerchandiseItem is initialized with the correct properties.
+        /// </summary>
+        [Test]
+        public void MerchandiseItem_Should_Have_Correct_Properties()
+        {
+            // Act
+            PageModel.OnGet();
+
+            // Assert
+            foreach (var item in PageModel.Merchandise)
+            {
+                Assert.That(item.Title, Is.Not.Null);
+                Assert.That(item.Description, Is.Not.Null);
+                Assert.That(item.ImageUrl, Is.Not.Null);
+                Assert.That(item.Hashtags, Is.Not.Null);
+                Assert.That(item.BuyLink, Is.Not.Null);
+                Assert.That(item.Price, Is.GreaterThan(0));
+            }
+        }
+
+        #endregion OnGet Tests
     }
 }
