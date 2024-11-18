7b3e22f6d1cc43030de61dfbd153cd70b2c6337e
Samarth Tanwar <stanwar@seattleu.edu>
2024-11-18 14:42:59 -0800

    added test code for rating in ProductList.razor.Test.cs file

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -65,5 +65,33 @@ namespace UnitTests.Components
             // Assert 
             Assert.That(pageMarkup.Contains("Naruto is an action-packed anime about Naruto Uzumaki, a young ninja aspiring to become Hokage, exploring themes of perseverance, friendship, and identity."), Is.EqualTo(true));
         }
+
+        [Test]
+        public void SubmitRating_Valid_ID_Click_Unstared_Should_Increment_Count_And_Check_Star()
+        {
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+            var id = "MoreInfoButton_jenlooper-cactus";
+            var page = RenderComponent<ProductList>();
+            var buttonList = page.FindAll("Button");
+            var button = buttonList.First(m => m.OuterHtml.Contains(id));
+            button.Click();
+            var buttonMarkup = page.Markup;
+            var starButtonList = page.FindAll("span");
+            var preVoteCountSpan = starButtonList[1];
+
+            var preVoteCoutString = preVoteCountSpan.OuterHtml;
+
+            var starButton = starButtonList.First(m => !string.IsNullOrEmpty(m.ClassName) && m.ClassName.Contains("fa fa-star"));
+            var preStarChange = starButton.OuterHtml;
+            starButton.Click();
+
+            buttonMarkup = page.Markup;
+            starButtonList = page.FindAll("span");
+            var postVoteCountSpan = starButtonList[1];
+
+            var postVoteCoutString = postVoteCountSpan.OuterHtml;
+            starButton = starButtonList.First(m => !string.IsNullOrEmpty(m.ClassName) && m.ClassName.Contains("fa fa-star checked"));
+            var postStarChange = starButton.OuterHtml;
+        }
     }
 }
