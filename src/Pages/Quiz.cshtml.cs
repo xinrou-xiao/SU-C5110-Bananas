511891940cc89467da86134eea0e1ab518cea917
ProZone <ssinha1@seattleu.edu>
2024-11-28 05:51:25 -0800

    Removed unused library in Quiz.cshtml.cs

--- src/Pages/Quiz.cshtml.cs
+++ src/Pages/Quiz.cshtml.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace ContosoCrafts.WebSite.Pages
 {
