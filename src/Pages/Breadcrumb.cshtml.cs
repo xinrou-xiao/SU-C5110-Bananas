c9e8f948c63f1e5562faaeb0d90a918d99b97c9c
Samarth Tanwar <stanwar@seattleu.edu>
2024-11-28 13:04:48 -0800

    Added Breadcrumb.cshtml.cs file to src\Pages folder

---
+++ src/Pages/Breadcrumb.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class BreadcrumbModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
