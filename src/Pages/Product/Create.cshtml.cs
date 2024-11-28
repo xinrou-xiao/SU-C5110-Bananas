0eb87fe923b3f9519c944c05340726d57dca340b
greenpoop <apple88501@gmail.com>
2024-11-27 21:27:45 -0800

    Updated OnPost function on Create.cshtml.cs

--- src/Pages/Product/Create.cshtml.cs
+++ src/Pages/Product/Create.cshtml.cs
@@ -37,18 +37,8 @@ namespace ContosoCrafts.WebSite.Pages.Product
         /// <summary>
         /// Processes POST requests to create a new product entry.
         /// </summary>
-        /// <param name="Product">The product to create.</param>
-        /// <param name="genre_dynamic">Genre string array.</param>
-        /// <param name="OTT_dynamic_platform">Platform name string array.</param>
-        /// <param name="OTT_dynamic_url">Platform URL string array.</param>
-        /// <param name="OTT_dynamic_icon">Platform icon string array.</param>
         /// <returns>Redirects to the Index page on success.</returns>
-        public IActionResult OnPost(
-            ProductModel Product,
-            string[] genre_dynamic,
-            string[] OTT_dynamic_platform,
-            string[] OTT_dynamic_url,
-            string[] OTT_dynamic_icon)
+        public IActionResult OnPost(ProductModel Product)
         {
             // Redirect to error page if model state is invalid
             if (!ModelState.IsValid)
@@ -61,50 +51,32 @@ namespace ContosoCrafts.WebSite.Pages.Product
             Product.Maker = "banana";
 
             // Process genre_dynamic array if it contains data
-            if (genre_dynamic.Length > 0)
+            if (Product.Genre.Length > 0)
             {
                 int count_Valid = 0;
 
                 // Count non-null entries in genre_dynamic
-                for (int i = 0; i < genre_dynamic.Length; i++)
+                for (int i = 0; i < Product.Genre.Length; i++)
                 {
-                    if (genre_dynamic[i] != null)
+                    if (Product.Genre[i] != null)
                     {
                         count_Valid++;
                     }
                 }
 
                 // Initialize Product.Genre with valid entries
-                Product.Genre = new string[count_Valid];
+                string[] valid_genre = new string[count_Valid];
                 var index = 0;
 
-                for (int i = 0; i < genre_dynamic.Length; i++)
+                for (int i = 0; i < Product.Genre.Length; i++)
                 {
-                    if (genre_dynamic[i] != null)
+                    if (Product.Genre[i] != null)
                     {
-                        Product.Genre[index] = genre_dynamic[i];
+                        valid_genre[index] = Product.Genre[i];
                         index++;
                     }
                 }
-            }
-
-            // Process OTT_dynamic arrays if platform names are provided
-            if (OTT_dynamic_platform.Length > 0)
-            {
-                for (int i = 0; i < OTT_dynamic_platform.Length; i++)
-                {
-                    if (OTT_dynamic_platform[i] != null) // Ensure platform name is provided
-                    {
-                        OTTModel OTT = new OTTModel
-                        {
-                            Platform = OTT_dynamic_platform[i],
-                            Url = OTT_dynamic_url[i],
-                            Icon = OTT_dynamic_icon[i]
-                        };
-
-                        Product.OTT.Add(OTT);
-                    }
-                }
+                Product.Genre = valid_genre;
             }
 
             // Create new product data and save to JSON file
