c51678c2ec80a08d60dc992455862d484cfb5a4a
greenpoop <apple88501@gmail.com>
2024-11-27 22:09:07 -0800

    Removed unused libraries on ProductTypeEnum.cs.Tests.cs

--- UnitTests/Models/ProductTypeEnum.cs.Tests.cs
+++ UnitTests/Models/ProductTypeEnum.cs.Tests.cs
@@ -1,10 +1,5 @@
 ﻿using ContosoCrafts.WebSite.Models;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UnitTests.Models
 {
