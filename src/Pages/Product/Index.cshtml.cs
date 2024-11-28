61dfbcde67e92438805d1822ad6c676859d8da50
ProZone <ssinha1@seattleu.edu>
2024-11-28 05:50:11 -0800

    Removed unused library in Product/Index.cshtml.cs

--- src/Pages/Product/Index.cshtml.cs
+++ src/Pages/Product/Index.cshtml.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
