faca39a9a3146bb1641faba541724d1492fd20e6
greenpoop <apple88501@gmail.com>
2024-11-27 21:34:42 -0800

    Updated unit tests on Delete.cshtml.Tests.cs

--- UnitTests/Pages/Product/Delete.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Delete.cshtml.Tests.cs
@@ -13,8 +13,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Routing;
-using System.Collections.Generic;
-using System;
 
 namespace UnitTests.Pages.Product.Delete
 {
@@ -104,23 +102,18 @@ namespace UnitTests.Pages.Product.Delete
             Assert.That(PageModel.ModelState.IsValid, Is.EqualTo(true));
             Assert.That(PageModel.Product.Id, Is.EqualTo("jenlooper-cactus"));
             Assert.That(PageModel.Product.Maker, Is.EqualTo("@jenlooper"));
-            
+
             Assert.That(PageModel.Product.Url, Is.EqualTo("https://naruto-official.com/en"));
             Assert.That(PageModel.Product.Title, Is.EqualTo("Naruto: The Journey of a Ninja Dreamer"));
             Assert.That(PageModel.Product.Description, Is.EqualTo("Naruto is an action-packed anime about Naruto Uzumaki, a young ninja aspiring to become Hokage, exploring themes of perseverance, friendship, and identity."));
-            Assert.That(PageModel.Product.Ratings, Is.EqualTo(new[] { 5, 5, 3, 2, 1, 5, 4, 5, 3 }));
+            Assert.That(PageModel.Product.Ratings, Is.EqualTo(new[] { 5, 5, 3, 2, 1, 5, 4, 5, 3, 5 }));
             Assert.That(PageModel.Product.Genre, Is.EqualTo(new[] { "Action", "Romance" }));
             Assert.That(PageModel.Product.Release, Is.EqualTo("1999"));
             Assert.That(PageModel.Product.Trailer, Is.EqualTo("https://www.youtube.com/embed/yeUpnIKt6k4"));
-            Assert.That(PageModel.Product.OTT[0].Platform, Is.EqualTo("Netflix"));
-            Assert.That(PageModel.Product.OTT[0].Url, Is.EqualTo("https://www.netflix.com/"));
-            Assert.That(PageModel.Product.OTT[0].Icon, Is.EqualTo("https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940"));
-            Assert.That(PageModel.Product.OTT[1].Platform, Is.EqualTo("Prime"));
-            Assert.That(PageModel.Product.OTT[1].Url, Is.EqualTo("https://www.amazon.com/Amazon-Video/b/?&node=2858778011&ref=dvm_MLP_ROWNA_US_1"));
-            Assert.That(PageModel.Product.OTT[1].Icon, Is.EqualTo("https://assets.aboutamazon.com/dims4/default/59e4166/2147483647/strip/true/crop/4454x2634+0+0/resize/365x216!/format/webp/quality/90/?url=https%3A%2F%2Famazon-blogs-brightspot.s3.amazonaws.com%2F4b%2F7f%2F4a4a80724a5e9a6b4a1931e8bf99%2Fprime-logo-rgb-prime-blue-master.png"));
+            Assert.That(PageModel.Product.Ott[0], Is.EqualTo(OttTypeEnum.Netflix));
+            Assert.That(PageModel.Product.Ott[1], Is.EqualTo(OttTypeEnum.Prime));
             Assert.That(PageModel.Product.Season, Is.EqualTo(5));
             Assert.That(PageModel.Product.CommentList, Is.Empty);
-
         }
 
         [Test]
