d520688a9f88f5b4116e613b3cc05ccf48050dac
vineetsom <84404299+vineetsom@users.noreply.github.com>
2024-12-02 17:22:16 -0800

    Added code to error handeling for all pages in startup.cs file

--- src/Startup.cs
+++ src/Startup.cs
@@ -63,6 +63,9 @@ namespace ContosoCrafts.WebSite
                 // Production error handler
                 app.UseExceptionHandler("/Error");
 
+            // redirect to error page on page not found
+            app.UseStatusCodePagesWithRedirects("/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
 
