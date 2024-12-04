ba5e85a60b9677d8a1b4870f1cb74e3f73389889
greenpoop <apple88501@gmail.com>
2024-12-03 19:44:27 -0800

    Removed unused library on ProductTypeEnum.cs

--- src/Models/ProductTypeEnum.cs
+++ src/Models/ProductTypeEnum.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ContosoCrafts.WebSite.Models
+﻿namespace ContosoCrafts.WebSite.Models
 {
     /// <summary>
     /// Types of products available.
